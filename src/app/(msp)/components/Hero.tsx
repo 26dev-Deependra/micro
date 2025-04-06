3e89fa9e37bd326590fd76a488696d90502d548b
Deependra <26dev.deependra@gmail.com>
2025-04-06 17:37:32 +0530

    v1.4

--- src/app/(msp)/components/Hero.tsx
+++ src/app/(msp)/components/Hero.tsx
@@ -9,7 +9,7 @@ export const Hero = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              AI-Driven Image Analytics
+              AI-Driven Biofilm Image Analytics
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-900 lg:text-xl xl:text-2xl dark:text-gray-300">
               MicroSpec is a service which aims to provide various Machine
